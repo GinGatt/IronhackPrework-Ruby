255ffc0d43daf78f77189ae7cc123195d14beec2
Gin Gattman <gingattman@gmail.com>
2015-09-01 17:11:01 -0400

    Iteration 2

--- FizzBuzz.rb
+++ FizzBuzz.rb
@@ -1,5 +1,6 @@
+=begin
 for i in 1..100
-	if i%3 == 0 && i%5 == 0 then
+	if i%3 == 0 && i%5 == 0
 		puts "FizzBuzz"
 	elsif i%5 == 0
 		puts "Buzz"
@@ -7,5 +8,19 @@ for i in 1..100
 		puts "Fizz"
 	else
 		puts "#{i}"
+	end
+end
+=end
+
+numbers = 1..100
+numbers.each do |num|
+	if num%3 == 0 && num%5 == 0
+		puts "FizzBuzz"
+	elsif num%5 == 0
+		puts "Buzz"
+	elsif num%3 == 0
+		puts "Fizz"
+	else
+		puts "#{num}"
 	end
 end
