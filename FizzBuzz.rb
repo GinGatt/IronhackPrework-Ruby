fa6a5fbf316c71a790195a9f0586718402f8ca1c
Gin Gattman <gingattman@gmail.com>
2015-09-02 17:11:47 -0400

    Iteration 3

--- FizzBuzz.rb
+++ FizzBuzz.rb
@@ -1,4 +1,5 @@
 =begin
+#iteration1 (Fizz @ multiples 3, Buzz @ multiples of 5)
 for i in 1..100
 	if i%3 == 0 && i%5 == 0
 		puts "FizzBuzz"
@@ -10,8 +11,9 @@ for i in 1..100
 		puts "#{i}"
 	end
 end
-=end
 
+
+#iteration 2 (method 2 for same goal)
 numbers = 1..100
 numbers.each do |num|
 	if num%3 == 0 && num%5 == 0
@@ -24,3 +26,29 @@ numbers.each do |num|
 		puts "#{num}"
 	end
 end
+=end
+
+#iteration 3 (+ Bang if number starts with 1)
+numbers = 1..100
+numbers.each do |num|
+	if num.to_s[1]=="1" && num%3 == 0 && num%5 == 0
+		puts "FizzBuzzBang"
+	elsif num%3 == 0 && num%5 == 0
+		puts "FizzBuzz"
+	elsif num%3 == 0 && num.to_s[0]=="1"
+		puts "FizzBang"
+	elsif num%5 == 0 && num.to_s[0]=="1"
+		puts "BuzzBang"
+	elsif num%5 == 0
+		puts "Buzz"
+	elsif num%3 == 0
+		puts "Fizz"
+	elsif num.to_s[0]=="1"
+		puts "Bang"
+	else
+		puts "#{num}"
+	end
+end
+
+
+
