dc9a17e983683f10dbe7caffaacda947e6127aa6
Gin Gattman <gingattman@gmail.com>
2015-09-10 16:31:03 -0400

    Add two sorting functions- a new array, and bubble.

--- wordsorting.rb
+++ wordsorting.rb
@@ -1,5 +1,14 @@
-#Receives UI of a sentence, returns words sorted alphabetically
+=begin
+#plain sorting function in which capitalization will matter
+def sentence_alphabatizer(sentence)
+	no_punct = sentence.gsub(/[^0-9a-z]/i, ' ')
+	sentence_array = no_punct.split(' ')
+	sentence_sorted = sentence_array.sort
+	puts sentence_sorted
+end
 
+#Second Sorting function that disregards capitalization
+#Inefficient; n!
 def custom_sort(string)
 	#remove punctuation 
 	no_punct = string.gsub(/[^0-9a-z]/i, ' ')
@@ -7,29 +16,47 @@ def custom_sort(string)
 	#split sentence string into an array based on whitespace char
 	sent_array = no_punct.split(/\s/)
 
+	new_array = []
 	#sorting loop to compare each element in array, so cap doesn't matter
-	i=1
+	while sent_array.length > 0
+		#set arbitrary minimum for comparison's sake
+		minimum = sent_array[0]
+		for elem in sent_array
+			if elem.upcase <= minimum.upcase
+				minimum = elem
+			end
+		end
+		#add elem[i] to end of new_array
+		new_array << minimum
+		#remove the original instance 
+		sent_array.delete(minimum)
+	end
+	print new_array
+end
+=end
+
+#Third sorting function that attempts a bubble sort method
+#n-1?
+def custom_sort(string)
+	#remove punctuation
+	#split sentence string into an array based on whitespace char
+	no_punct = string.gsub(/[^0-9a-z]/i, ' ')
+	sent_array = no_punct.split(/\s/)
+	
+	i=0
 	while i < sent_array.length
-		if sent_array[i].upcase < sent_array[(i-1)].upcase
-			#add elem[(i-1)] to end of array
-			sent_array.push(sent_array[(i-1)])
-			#remove the original instance 
-			sent_array.delete_at(i-1)
+		j=1
+		while j < sent_array.length
+			if sent_array[j].upcase <= sent_array[(j-1)].upcase
+				sent_array.insert(j-1, sent_array[j])
+				sent_array.delete_at(j+1)
+			end
+			j += 1
 		end
 		i += 1
 	end
 	print sent_array
 end
-
-=begin 
-#previous sorting in which capitalization will matter
-def sentence_alphabatizer(sentence)
-	no_punct = sentence.gsub(/[^0-9a-z]/i, ' ')
-	sentence_array = no_punct.split(' ')
-	sentence_sorted = sentence_array.sort
-	puts sentence_sorted
-end
-=end
 
 puts "Give me a sentence!"
 sentence = gets.chomp
